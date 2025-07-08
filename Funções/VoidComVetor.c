73689cb52065172bd7ad2e110d397652ffb601f4
Rogerllc <rogerlleite.rn@hotmail.com>
2025-07-08 10:49:04 -0300

    Void

---
+++ Funções/VoidComVetor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void numeros(){
+    int num[6];
+    for(int i = 1; i <= 6; i++){
+   printf("\nNumeros digitados: %d\n", num[6]);
+    }
+}
+
+int main(){
+    int num[6];
+    for(int i = 0; i < 6; i++){
+        printf("Informe um numero: ");
+        scanf("%d", &num[i]);
+}
+numeros();
+return 0;
+}
